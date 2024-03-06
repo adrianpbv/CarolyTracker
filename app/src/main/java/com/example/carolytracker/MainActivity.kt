82007873f1fb921ff1c27c53b01c0ec4638b1326
adrianj <ajperez@hitpromo.net>
2024-03-06 06:54:09 -0500

    add ActivityScreen.kt and GoalScreen.kt

--- app/src/main/java/com/example/carolytracker/MainActivity.kt
+++ app/src/main/java/com/example/carolytracker/MainActivity.kt
@@ -14,9 +14,11 @@ import androidx.navigation.compose.rememberNavController
 import com.example.carolytracker.navigation.navigate
 import com.example.carolytracker.ui.theme.CaloryTrackerTheme
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.activity.ActivityScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import com.example.onboarding_presentation.age.AgeScreen
+import com.example.onboarding_presentation.goal.GoalScreen
 import com.example.onboarding_presentation.height.HeightScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -61,10 +63,10 @@ class MainActivity : ComponentActivity() {
 
                         }
                         composable(Route.ACTIVITY) {
-
+                            ActivityScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.GOAL) {
-
+                            GoalScreen(onNavigate = navController::navigate)
                         }
 
                         composable(Route.TRACKER_OVERVIEW) {
